194d82ee20e562ce7099fc0568f94470b852043a
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-05-08 14:00:46 +0530

    Intro to Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3+3) // we are using NODEJS, not browser.
+
+let name = "Lakhan"
+let age = 23
+let isLoggedIn = false
+let profession;
+
+
+// Primitive Datatypes
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// Non-primitive datatype
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
