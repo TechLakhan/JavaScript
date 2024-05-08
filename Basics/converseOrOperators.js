f9eca8c50b518e7db1e665195477033c13553c3c
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-05-08 15:59:18 +0530

    Learnt about operators in JS

--- Basics/converseOrOperators.js
+++ Basics/converseOrOperators.js
@@ -25,6 +25,37 @@ let confirmLogin = Boolean(isLoggedIn);
 let someNumber = 2000
 
 let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// ******************************** Operation **************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let greetings = "hello "
+let name = "Lucky"
+// console.log(greetings + name);
+
+// console.log("1" + 2); // => 12
+// console.log(4 + "5"); // => 45
+// console.log("1" + 2 + 2); // => 122
+// console.log(1 + 2 + "2"); // => 32
+
+// console.log(true);
+
+// postfix operator => here value remains the same before output.
+let x = 1
+const y = x++;
+
+console.table([x, y]);
+
+// prefix operator => Value changes with the output or before output.
+
+let a = 10
+const b = ++a;
+
+console.table([a, b]);
+
+
