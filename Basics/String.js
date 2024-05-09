2895d2240bae904a6ed2cb8bfac3233fa01a0722
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-05-09 16:06:20 +0530

    String in JS

---
+++ Basics/String.js
@@ -0,0 +1,32 @@
+const name = "Lakhan-Patil"
+const repoCount = 17
+
+// console.log("Hi, My name is " + name + ". " + "I have " + repoCount + " git repos.");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // -> newer method to write Strings
+
+// console.log(name.__proto__);
+
+// console.log(name.toLowerCase());
+
+// console.log(name.indexOf('k'));
+
+const newString = name.substring(7,12);
+
+// console.log(newString);
+
+const anotherString = "    Lakhan    "
+
+// console.log(anotherString.trim());
+
+const url = "https://lakhan.com/lakhan%20patil"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('lakhan'));
+
+console.log(name.split('-'));
+
+
+
+
