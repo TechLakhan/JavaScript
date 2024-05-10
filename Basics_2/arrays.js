361d2ee157c40a00cde9e74bb7abea5ec67b569f
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-05-10 16:36:55 +0530

    Arrays-1

---
+++ Basics_2/arrays.js
@@ -0,0 +1,36 @@
+// arrays
+
+const myArr = [0, 1, 4, 8, 13]
+
+const movies = ["KGF", "Animal", "Phir Hera Pheri", "Pushpa"]
+
+// myArr.push(6);
+// myArr.push(100)
+
+// myArr.pop();
+
+// myArr.unshift(23);
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr.includes(8));
+
+// console.log(myArr.indexOf(8))
+
+// ************* SLICE & SPLICE ********************
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr.toA);
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C ", myArr);
