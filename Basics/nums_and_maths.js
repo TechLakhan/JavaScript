92a4e737f61e97ef6ab1ca39d3735310d4369c50
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2024-05-10 12:02:10 +0530

    Maths & Numbers in JS

---
+++ Basics/nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balance = new Number(40800)
+console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8745
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+// ++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-172));
+// console.log(Math.sqrt(64));
+// console.log(Math.max(11, 90, 25, 37, 59));
+
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min);
+
+
+
